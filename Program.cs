3e95d5a5db27358a88ee39fd3e7e16a90a7e8b4b
Sukharevich Vladislav <vladoslav.sukharevich3@gmail.com>
2021-03-11 11:07:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task4_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите точность: ");
+            double eps = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double aPrev = 0;
+            double a = 0;
+            double n = 0;
+            while(Math.Abs(aPrev - a) < eps)
+            {
+                aPrev = 3 + (1 / Math.Pow(2, n)) * Math.Pow(Math.Cos(aPrev - x), 2);
+                n++;
+                a += 3 + (1 / Math.Pow(2, n)) * Math.Pow(Math.Cos(aPrev - x), 2);
+                Console.WriteLine(Math.Abs(aPrev - a));
+                Console.WriteLine(eps);
+            }
+            Console.WriteLine($"Сумма: {a}");
+        }
+    }
+}
